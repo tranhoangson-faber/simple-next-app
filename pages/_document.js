a8e6d11ecf35a36a1f37e9c0da56ca466dc55e28
SonTranFaber <tranhoangson@fabercompany.co.jp>
2024-10-09 11:52:38 +0700

    fix 4

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                <link rel='icon' href='/simple-next-app/favicon.ico' />
-                <Script src='/simple-next-app/script' strategy='beforeInteractive' />
+                <Script id='mierucaOptimizejs' strategy='lazyOnload'>
+                    {`window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
+            (function () {
+              var fjs = document.createElement('script'); 
+              fjs.type = 'text/javascript'; 
+              fjs.async = true; 
+              fjs.id = "fjssync"; 
+              var timestamp = new Date; 
+              fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
+              var x = document.getElementsByTagName('script')[0]; 
+              x.parentNode.insertBefore(fjs, x);
+            })();`}
+                </Script>
             </Head>
             <body>
                 <Main />

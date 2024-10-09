8ae750aa37e76ee229df14e6e70b9a69d7672010
SonTranFaber <tranhoangson@fabercompany.co.jp>
2024-10-09 11:33:24 +0700

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                <Script src='script.js' strategy='beforeInteractive' />
+                <Script src='/script.js' strategy='beforeInteractive' />
             </Head>
             <body>
                 <Main />

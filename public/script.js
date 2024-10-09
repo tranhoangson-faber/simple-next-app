80399c62eaa1ab91d8dd95430832ee555ad6cc8c
SonTranFaber <tranhoangson@fabercompany.co.jp>
2024-10-09 11:32:26 +0700

    add script

---
+++ public/script.js
@@ -0,0 +1,6 @@
+<script type="text/javascript" id="mierucaOptimizejs">
+window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
+(function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
+fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
+var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
+</script>

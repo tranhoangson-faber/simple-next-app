88ec9c37bc89e08df9536c759b4625f96cf94329
NguyenLeMinhFB <124113316+NguyenLeMinhFB@users.noreply.github.com>
2024-10-09 16:18:12 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
+// LOCAL_HOST
+// <script type="text/javascript" id="mierucaOptimizejs">
+// window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
+// (function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
+// fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
+// var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
+// </script>
+
+// DEV
+<!-- Mieruca Optimize Tag -->
 <script type="text/javascript" id="mierucaOptimizejs">
-window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
+window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1864963081]);
 (function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
-fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
+fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=' + timestamp.getTime();
 var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
 </script>
+<!-- End Mieruca Optimize Tag -->

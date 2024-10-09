1eec9750b00718cc090c621382440e0176dfd8f3
NguyenLeMinhFB <124113316+NguyenLeMinhFB@users.noreply.github.com>
2024-10-09 16:23:32 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,17 +6,18 @@ export default function Document() {
         <Html lang='en'>
             <Head>
                 <Script id='mierucaOptimizejs' strategy='beforeInteractive'>
-                    {`window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
-            (function () {
-              var fjs = document.createElement('script'); 
-              fjs.type = 'text/javascript'; 
-              fjs.async = true; 
-              fjs.id = "fjssync"; 
-              var timestamp = new Date; 
-              fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
-              var x = document.getElementsByTagName('script')[0]; 
-              x.parentNode.insertBefore(fjs, x);
-            })();`}
+             window.__optimizeid = window.__optimizeid || [];
+__optimizeid.push([1864963081]);
+(function() {
+	var fjs = document.createElement('script');
+	fjs.type = 'text/javascript';
+	fjs.async = true;
+	fjs.id = "fjssync";
+	var timestamp = new Date;
+	fjs.src = 'https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=' + timestamp.getTime();
+	var x = document.getElementsByTagName('script')[0];
+	x.parentNode.insertBefore(fjs, x);
+})();
                 </Script>
             </Head>
             <body>

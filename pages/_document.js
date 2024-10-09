547e2646fc02835ca8ca2587bdea739f59731022
NguyenLeMinhFB <124113316+NguyenLeMinhFB@users.noreply.github.com>
2024-10-09 11:17:42 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
 <script type="text/javascript" id="mierucaOptimizejs">
 window.__optimizeid = window.__optimizeid || [];__optimizeid.push([1163282239]);
 (function () {var fjs = document.createElement('script');fjs.type = 'text/javascript';
-fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=' + timestamp.getTime();
+fjs.async = true;fjs.id = "fjssync";var timestamp = new Date;fjs.src = 'http://localhost:8082/mieruca-optimize?v=' + timestamp.getTime();
 var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(fjs, x);})();
 </script>
 <!-- End Mieruca Optimize Tag -->

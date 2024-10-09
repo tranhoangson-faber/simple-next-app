5f59a851607cbeceffbee21dad03f70c19c931f4
NguyenLeMinhFB <124113316+NguyenLeMinhFB@users.noreply.github.com>
2024-10-09 16:26:14 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,8 @@ export default function Document() {
             <Head>
                 <Script id='mierucaOptimizejs' strategy='beforeInteractive'>
              window.__optimizeid = window.__optimizeid || [];
-__optimizeid.push([1864963081]);
-(function() {
+             __optimizeid.push([1864963081]);
+        (function() {
 	var fjs = document.createElement('script');
 	fjs.type = 'text/javascript';
 	fjs.async = true;
@@ -17,7 +17,7 @@ __optimizeid.push([1864963081]);
 	fjs.src = 'https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=' + timestamp.getTime();
 	var x = document.getElementsByTagName('script')[0];
 	x.parentNode.insertBefore(fjs, x);
-})();
+        })();
                 </Script>
             </Head>
             <body>

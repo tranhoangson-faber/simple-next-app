18cd5e1e0071c03886f9d05e7a50b03814c8ad60
NguyenLeMinhFB <124113316+NguyenLeMinhFB@users.noreply.github.com>
2024-10-11 13:09:31 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -28,3 +28,19 @@ __optimizeid.push([1864963081]);
 	x.parentNode.insertBefore(fjs, x);
 })();`}
 </Script>
+
+// PROD
+<Script id='mierucaOptimizejs' strategy='beforeInteractive'>
+{`window.__optimizeid = window.__optimizeid || [];
+__optimizeid.push([1194539479]);
+(function() {
+	var fjs = document.createElement('script');
+	fjs.type = 'text/javascript';
+	fjs.async = true;
+	fjs.id = "fjssync";
+	var timestamp = new Date;
+	fjs.src = 'https://opt.mieru-ca.com/service/js/mieruca-optimize.js?v=' + timestamp.getTime();
+	var x = document.getElementsByTagName('script')[0];
+	x.parentNode.insertBefore(fjs, x);
+})();`}
+</Script>
